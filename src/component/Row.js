6b57fcacfd6309ac1e67a461a8a6ed6c696ba4ac
Shubham Kumar <shubhamsre1997@gmail.com>
2020-09-26 12:55:31 +0530

    making movie Row list

--- src/component/Row.js
+++ src/component/Row.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./Row.css";
 import axios from "../axios"; // we are importing instance as a axios from axios.js file
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
   // for geting a list of movies , initially empty movies array
   const [movies, setMovies] = useState([]);
@@ -11,8 +13,8 @@ function Row({ title, fetchUrl }) {
       const request = await axios.get(fetchUrl); // same instance.get("/foo-bar"); from axios.js file
       // https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213
 
-      // console.log(request);
-      setMovies(request.data.result);
+      //   console.log(request);
+      setMovies(request.data.results);
       return request;
     }
 
@@ -21,11 +23,24 @@ function Row({ title, fetchUrl }) {
   }, [fetchUrl]);
 
   return (
-    <div>
+    <div className="row">
       <h1>{title}</h1>
       {/* title */}
+      <div className="row__posters">
+        {/* row posters */}
 
-      {/* container -> poster */}
+        {movies.map((movie) => {
+          // movie.poster_path="/ta5oblpMlEcIPIS2YGcq9XEkWK2.jpg" ,not a full url have to add base url
+          return (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
