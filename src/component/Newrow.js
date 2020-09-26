7bbae6fe9ac5cb6e7602e8b7c4c5d92d32bdc610
Shubham Kumar <shubhamsre1997@gmail.com>
2020-09-26 22:18:09 +0530

    to add switch fun. made a Newrow component all row

---
+++ src/component/Newrow.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../App.css";
+import Row from "./Row";
+import request from "../request";
+import Banner from "./Banner";
+function Newrow() {
+  return (
+    <div className="Newrow">
+      <Banner />
+
+      <Row title="NETFLIX ORIGINAL" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Trending Movies" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default Newrow;
